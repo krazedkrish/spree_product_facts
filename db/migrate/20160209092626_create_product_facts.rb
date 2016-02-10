e495fe6b5ce9cf57d3e1011171c0c3ad4d3ab730
krazedkrish <krazedkrish@gmail.com>
2016-02-10 14:14:59 +0545

    fix translation table issue

--- db/migrate/20160209092626_create_product_facts.rb
+++ db/migrate/20160209092626_create_product_facts.rb
@@ -1,9 +1,13 @@
 class CreateProductFacts < ActiveRecord::Migration
-  def change
+  def up
     create_table :spree_product_facts do |t|
-      t.text :facts
-
       t.timestamps null: false
     end
+    Spree::ProductFact.create_translation_table! :facts => :text
+  end
+
+  def down
+    drop_table :spree_product_facts
+    Spree::ProductFact.drop_translation_table!
   end
 end
