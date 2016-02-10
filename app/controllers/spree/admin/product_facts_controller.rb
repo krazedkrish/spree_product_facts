16dfff8ae8b35c5d866919cef7a20b368c6e0425
krazedkrish <krazedkrish@gmail.com>
2016-02-10 22:15:36 +0545

    update bug fixes

--- app/controllers/spree/admin/product_facts_controller.rb
+++ app/controllers/spree/admin/product_facts_controller.rb
@@ -13,30 +13,26 @@ class Spree::Admin::ProductFactsController < Spree::Admin::ResourceController
 
   def update
     product_fact = Spree::ProductFact.find(params[:id])
-    product_fact_translations_attribute = update_product_fact_attribute["translations_attributes"]
-    product_fact_translations_attribute.map do |k,v|
-      v.slice("locale","facts")
+    unless update_product_fact_attribute["facts"].nil?
+      product_fact.update update_product_fact_attribute
+    else
+      product_fact_translations_attribute = update_product_fact_attribute["translations_attributes"]
+      product_fact_translations=product_fact_translations_attribute.map do |k,v|
+        v.slice("locale","facts")
+      end
+      product_fact_translations.each { |pf| product_fact.attributes=pf }
+      product_fact.save
     end
-    product_fact_translations_attribute.each { |pf| product_fact.attributes=pf }
-    product_fact_translations_attribute.save
     redirect_to spree.admin_product_facts_path
   end
 
   private
-  def update_product_fact_attribute_test
-    params.require(:product_fact).permit(permitted_params)
-  end
-
-  def permitted_params_test
-    [translation: [:id, :locale, :facts]]
-  end
-
   def update_product_fact_attribute
     params.require(:product_fact).permit(permitted_params)
   end
 
   def permitted_params
-    [translations_attributes: [:id, :locale, :facts]]
+    [:facts, product_ids: [], translations_attributes: [:id, :locale, :facts, :product_ids]]
   end
 
 end
