d15f809815af40dc37c1a5d34cbb21184ffbfc5d
krazedkrish <krazedkrish@gmail.com>
2016-02-11 11:37:46 +0545

    adds validation

--- app/models/spree/product_fact.rb
+++ app/models/spree/product_fact.rb
@@ -1,4 +1,9 @@
 class Spree::ProductFact < ActiveRecord::Base
+  validates :facts, { :length => { :in => 0..255 },
+                      :presence => true,
+                      :allow_blank => true,
+                      :allow_nil => false,
+                    }
   translates :facts, :fallbacks_for_empty_translations => true
 
   has_many :products
